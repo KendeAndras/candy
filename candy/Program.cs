e26431a002072eb60e754d63a710dfcb79956f90
Korcsmáros Kende András <f26xc0@INF.ELTE.HU>
2023-12-19 16:36:55 +0100

    1.feladat kezdve

---
+++ candy/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace candy
+{
+    internal class Program
+    {
+        struct Candy
+        {
+            public int manufacter;
+            public int type;
+            public int price;
+        }
+
+        static string dataInput ()
+        {
+            string inputText = Console.ReadLine ();
+            return inputText; 
+        }
+
+        static string splitData(string inputText)
+        {
+            inputText.Split(" ");
+            return inputText;
+        }
+
+        static Candy[] candyBlockCreate(int count)
+        {
+            string inputText;
+            Candy[] block = new Candy[count + 1];
+            for (int i = 1; i <= count; i++)
+            {
+                inputText = dataInput();
+                string row = splitData(inputText);
+                block[i].manufacter = row[0];
+                block[i].type = row[1];
+                block[i].price = row[2];
+            }
+            return block;
+
+        }
+
+        static void Main(string[] args)
+        {
+            string input = dataInput ();
+            string split = splitData (input);
+            int sale = Convert.ToInt32(split[0]);
+            int manfCount = Convert.ToInt32(split[1]);
+            int typeCount = Convert.ToInt32(split[2]);
+
+            Candy[] candies = candyBlockCreate(sale);
+
+            //1. feladat
+            int manfOfMinPr = 0;
+            int typeOfMinPr = 0;
+            for (int i = 1; i <= sale; i++)
+            {
+                if (candies[i].price < candies[i+1].price)
+                {
+                    manfOfMinPr = i;
+                    
+                }
+            }
+            
+
+
+        }
+    }
+}
